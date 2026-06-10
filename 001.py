2a295b7d65af7c9f372726135d46f0bba130afd2
bp4643637-cyber <bp4643637@gmail.com>
2026-06-10 12:08:29 -0600

    Update 001.py

--- 001.py
+++ 001.py
@@ -5,3 +5,5 @@ num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 r = num1 + num2
 print("La respuesta es", r)
+r = num1 - num2
+print("la respuesta es",r)
